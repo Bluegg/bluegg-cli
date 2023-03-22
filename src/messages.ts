b89affe4a28f74de3e3e20410bb3ef43e90d07a9
Charlie Dowley <dowleycharlotte@hotmail.co.uk>
2023-03-22 12:32:11 +0000

    Update Message

--- src/messages.ts
+++ src/messages.ts
@@ -7,7 +7,7 @@ import ErrorMessage from "./libraries/messages/ErrorMessage.ts";
 export function missingSubcommand() {
 	new ErrorMessage(
 		`Enter a recognised subcommand or argument(s). Use ${bold("--help")} for help.`,
-		true,
+		true
 	);
 }
 
@@ -18,8 +18,10 @@ export function missingSubcommand() {
  */
 export function invalidSubcommand(subcommand: string) {
 	new ErrorMessage(
-		`"${subcommand}" is not a recognised subcommand. Use ${bold("--help")} for help.`,
-		true,
+		`"${subcommand}" is ${bold("--not")} a recognised subcommand. Use ${bold(
+			"--help"
+		)} for help.`,
+		true
 	);
 }
 
@@ -29,7 +31,7 @@ export function invalidSubcommand(subcommand: string) {
 export function noPossibleSubcommands() {
 	new ErrorMessage(
 		`There are no possible subcommands available. Use ${bold("--help")} for help.`,
-		true,
+		true
 	);
 }
 
@@ -39,6 +41,6 @@ export function noPossibleSubcommands() {
 export function missingArguments() {
 	new ErrorMessage(
 		`At least one required argument is missing. Use ${bold("--help")} for help.`,
-		true,
+		true
 	);
 }
